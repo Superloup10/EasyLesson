c60d8c0d86c37db7df52f5ba5fc15ed4139284cf
Superloup10 <Superloup10@gmail.com>
2014-09-27 01:16:18 +0200

    update project

--- src/fr/wolf/easylesson/MainActivity.java
+++ src/fr/wolf/easylesson/MainActivity.java
@@ -2,6 +2,7 @@ package fr.wolf.easylesson;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity
 {
@@ -9,6 +10,13 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_main.xml);
+        this.setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        this.getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
